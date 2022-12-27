9b79212205d21738660ffbbc5228c1cbc8c06bd2
Bart Vandeputte <mail@bartvandeputte.be>
2022-12-27 18:29:59 +0100

    Git submodule compatibility

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ use Kirby\Cms\App as Kirby;
 
 @include_once __DIR__.'/vendor/autoload.php';
 
+@include_once __DIR__.'/src/Guards/TurnstileGuard.php';
+@include_once __DIR__.'/src/helpers.php';
+
 Kirby::plugin('anselmh/uniform-turnstile', [
   'options' => [
     'siteKey' => '',
